ea3391fcbc73246329cc05bee1887b3874372ea0
Jean Gonçalves <jean.gnc@gmail.com>
2020-01-16 13:41:11 -0300

    Add a scoreboard

---
+++ sync_scoreboard.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'pg'
+require 'json'
+
+uri = 'postgres://postgres:postgres@localhost:5656/cdp_core_db'
+
+query = <<~SQL
+  select string_field_value_1 as email, string_field_value_15 as score
+  from entities
+  where string_field_value_1 is not null
+  order by 2;
+SQL
+
+loop do
+  pg_conn = PG.connect(uri)
+
+  results = pg_conn.exec(query).to_a.map do |r|
+    r['score'] = r['score'].to_i
+    r
+  end
+
+  File.open("scores.js","w+") do |f|
+    f.write("window.scores = #{results.to_json}")
+  end
+
+  pg_conn.close
+  sleep 1
+end
